e16d678bd5b6e56ef4aa6daf3e9ab7f9c7580434
Martha <martha_15_7@outlook.com>
2021-05-17 10:11:56 -0500

    Corrección de la duplicación del org.aspectj
    Corrección de la duplicación del org.aspectj

--- app/src/androidTest/java/com/example/trabajosexpres/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/trabajosexpres/ExampleInstrumentedTest.kt
@@ -1,13 +1,10 @@
 package com.example.trabajosexpres
 
 import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
