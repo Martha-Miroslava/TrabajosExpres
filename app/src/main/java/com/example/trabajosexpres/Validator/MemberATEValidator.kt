9ea8c5c6d274efb9ab0d31abd5456c9ada223f05
Martha <martha_15_7@outlook.com>
2021-06-20 14:35:20 -0500

    Finalización del la funcionalidad crear cuenta
    Finalización del la funcionalidad crear cuenta

--- app/src/main/java/com/example/trabajosexpres/Validator/MemberATEValidator.kt
+++ app/src/main/java/com/example/trabajosexpres/Validator/MemberATEValidator.kt
@@ -40,7 +40,7 @@ class MemberATEValidator : AbstractValidator<MemberATE> {
             .withMessage("Ingresa un email correcto")
 
         ruleFor(MemberATE::dateBirth)
-            .must(isDate("yyyy-MM-dd"))
+            .must(StringPredicate.stringMatches("([12]\\d{3}/(0[1-9]|1[0-2])/(0[1-9]|[12]\\d|3[01]))"))
             .withMessage("Ingresa una fecha correcta")
 
         ruleFor(MemberATE::username)
