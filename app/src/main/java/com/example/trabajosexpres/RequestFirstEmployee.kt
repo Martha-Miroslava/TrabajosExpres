a7bf308cb2ccbb4a6501b483fa77536d76ac817b
Alemin <ale_200200@hotmail.com>
2021-06-20 15:25:54 -0500

    Cambios de nombre de Request class
    Cambios de nombre de Request class

--- app/src/main/java/com/example/trabajosexpres/RequestFirstEmployee.kt
+++ app/src/main/java/com/example/trabajosexpres/RequestFirstEmployee.kt
@@ -3,7 +3,7 @@ package com.example.trabajosexpres
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class Request_First_Employee: AppCompatActivity() {
+class RequestFirstEmployee: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.request_fist_employee)
